ac95f590634f5eb80dc569f9c8b68dbdd821f618
julianlau1998 <Julian.lau@aol.com>
2023-03-29 21:57:40 +0200

    matomo

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,8 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    ['nuxt-matomo', { matomoUrl: 'https://howworks.matomo.cloud/', siteId: 2 }],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
